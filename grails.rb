199e4fe91d4a184206f96419842cb56e0c10918f
Ken Herner <chosenken@gmail.com>
2013-12-07 23:25:54 -0500

    Updated grails to 2.3.4

---
+++ grails.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Grails < Formula
+  homepage 'http://grails.org'
+  url 'http://dist.springframework.org.s3.amazonaws.com/release/GRAILS/grails-2.3.4.zip'
+  sha1 '13c39936f1b820ebb13e03fd56cc617b0c68eaee'
+
+  def install
+    rm_f Dir["bin/*.bat", "bin/cygrails", "*.bat"]
+    prefix.install_metafiles
+    libexec.install Dir['*']
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  def caveats; <<-EOS.undent
+    The GRAILS_HOME directory is:
+      #{opt_prefix}/libexec
+    EOS
+  end
+end
